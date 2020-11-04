433d791e6604f4774ddc207a5e243db1ddfca709
Emma Honkamaa <ematan@iki.fi>
2020-11-04 12:06:55 +0200

    9.19

--- osa9/patientor/patientor-backend/src/utils/index.ts
+++ osa9/patientor/patientor-backend/src/utils/index.ts
@@ -1,7 +1,14 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { newPatientEntry, Gender } from '../types';
+import {  newPatientEntry,
+          Gender,
+          Entry,
+          HospitalEntry,
+          OccupationalHealthcareEntry,
+          HealthCheckEntry,
+          BaseEntry,
+       } from '../types';
 
 const isString = (text: any): text is string => {
   return typeof text === 'string' || text instanceof String;
@@ -50,20 +57,81 @@ const parseOccupation = (occupation: any): string => {
   return occupation;
 };
 
-/*const isEntry = (entry: any): entry is Entry => {
-  return (entry instanceof Entry);
+
+const isBaseEntry = (entry: any): entry is BaseEntry => {
+  if (
+    entry.id && isString(entry.id) &&
+    entry.description && isString(entry.description) &&
+    entry.date && isDate(entry.date) &&
+    entry.specialist && isString(entry.specialist)
+  ) {
+    if (entry.diagnosisCodes){
+      return isString(entry.diagnosisCodes);
+    }
+    return true;
+  }
+  return false;
+};
+
+const isHospitalEntry = (entry: any): entry is HospitalEntry => {
+  if (entry.type && entry.type === 'Hospital'){
+    return(
+      entry.discharge &&
+      Object.keys(entry.discharge).includes('date') &&
+      Object.keys(entry.discharge).includes('criteria')
+    );
+  }
+  return false;
+};
+
+const isOccupationalHealthcareEntry = (entry: any): entry is OccupationalHealthcareEntry => {
+  if (entry.type && entry.type === 'OccupationalHealthcare' && entry.employerName) {
+      if (entry.sickLeave) {
+        return(
+          Object.keys(entry.sickLeave).includes('startDate') &&
+          Object.keys(entry.sickLeave).includes('endDate')
+        );
+      }
+    return true;
+  }
+  return false;
+};
+
+const isHealthCheckEntry = (entry: any): entry is HealthCheckEntry => {
+  if (entry.type && entry.type === 'HealthCheck'){
+    if(!(entry.healthCheckRating === undefined)){
+      return (
+        0 <= entry.healthCheckRating &&
+        entry.healthCheckRating < 4
+      );
+    }
+  }
+  return false;
+};
+
+
+const isEntry = (entry: any): entry is Entry => {
+  if (!isBaseEntry) throw new Error('not even a base entry :(');
+  return (
+    isHospitalEntry(entry) ||
+    isOccupationalHealthcareEntry(entry) ||
+    isHealthCheckEntry(entry)
+  );
 };
 
 const parseEntries = (entries: any): Entry[] => {
   if (!entries || !Array.isArray(entries)) {
     throw new Error('Entries missing');
   }
-  const isValid = entries.every(e => isEntry(e));
-  if (!entries || !isValid) {
-    throw new Error('Entries missing');
-  }
-  return entries;
-}*/
+  return entries.map(e => {
+    console.log(e);
+    if (!isEntry(e)){
+      throw new Error('Not an entry');
+    } else {
+      return e;
+    }
+  });
+};
 
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -74,7 +142,7 @@ const toNewPatientEntry = (object: any): newPatientEntry => {
     dateOfBirth: parseDOB(object.dateOfBirth),
     ssn: parseSSN(object.ssn),
     occupation: parseOccupation(object.occupation),
-    entries: []
+    entries: parseEntries(object.entries)
   };
   return newPatient;
 };
