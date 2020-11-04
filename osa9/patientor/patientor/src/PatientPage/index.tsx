88a95cb91f170b4f27a3b47193c9c5131a1f270b
Emma Honkamaa <ematan@iki.fi>
2020-11-04 10:33:45 +0200

    9.18

--- osa9/patientor/patientor/src/PatientPage/index.tsx
+++ osa9/patientor/patientor/src/PatientPage/index.tsx
@@ -5,6 +5,8 @@ import { Icon } from 'semantic-ui-react'
 
 import { apiBaseUrl } from '../constants';
 import { Patient, Gender } from '../types';
+import { setFetchedPatient } from "../state";
+import { useStateValue } from '../state';
 
 const GenderIcon: React.FC<{gender: Gender}> = ({gender}) => {
   switch (gender){
@@ -19,20 +21,26 @@ const PatientPage: React.FC = () => {
 
   const { id } = useParams<{id: string}>();
   const [patient, setPatient] = useState<Patient | undefined>();
+  const [{patientDetails}, dispatch] = useStateValue();
 
   useEffect(() => {
     const getPatient = async () => {
       try {
         const result = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
         console.log(result.data);
-        //dispatch(result.data);
+        dispatch(setFetchedPatient(result.data));
         setPatient(result.data);
       }catch (e){
         console.log(e);
       }
     };
+    if (patientDetails[id]) {
+      setPatient(patientDetails[id]);
+    } else {
+      getPatient();
+    }
     getPatient();
-  }, [id])
+  }, [id, patientDetails, dispatch],)
 
   console.log(patient);
   if(!patient) return <div>Loading</div>;
