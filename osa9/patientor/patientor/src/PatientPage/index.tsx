50cca5565089224e04524513c62b98e98c335221
Emma Honkamaa <ematan@iki.fi>
2020-11-05 17:02:59 +0200

    9.21

--- osa9/patientor/patientor/src/PatientPage/index.tsx
+++ osa9/patientor/patientor/src/PatientPage/index.tsx
@@ -4,8 +4,8 @@ import { useParams } from 'react-router-dom';
 import { Icon } from 'semantic-ui-react'
 
 import { apiBaseUrl } from '../constants';
-import { Patient, Gender } from '../types';
-import { setFetchedPatient } from "../state";
+import { Patient, Gender, Entry, Diagnosis } from '../types';
+import { setFetchedPatient, setDiagnoses } from "../state";
 import { useStateValue } from '../state';
 
 const GenderIcon: React.FC<{gender: Gender}> = ({gender}) => {
@@ -17,33 +17,68 @@ const GenderIcon: React.FC<{gender: Gender}> = ({gender}) => {
   }
 }
 
+type EntryProps = {
+  entry: Entry,
+  diagnoses: {[code: string]: Diagnosis}
+};
+
+const EntryRow: React.FC<EntryProps> = ({entry, diagnoses}) => {
+  console.log('this', diagnoses)
+
+  const diagnosis = entry.diagnosisCodes?.map(d => {
+    const diagnosisData = diagnoses[d];
+    return <li key={d}><span>{diagnosisData.code} {diagnosisData.name}
+          {diagnosisData.latin ? <p>(<i>{diagnosisData.latin}</i>)</p> : null} </span></li>
+  });
+
+  return (
+    <div>
+      {entry.date} {entry.description} <br />
+      <ul>{diagnosis}</ul>
+    </div>
+  )
+}
+
 const PatientPage: React.FC = () => {
 
   const { id } = useParams<{id: string}>();
   const [patient, setPatient] = useState<Patient | undefined>();
-  const [{patientDetails}, dispatch] = useStateValue();
+  const [{patientDetails, diagnoses}, dispatch] = useStateValue();
 
   useEffect(() => {
     const getPatient = async () => {
       try {
         const result = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
-        console.log(result.data);
         dispatch(setFetchedPatient(result.data));
-        setPatient(result.data);
-      }catch (e){
+      } catch (e) {
         console.log(e);
       }
     };
+    const fetchDiagnoses = async () => {
+      try {
+        const result = await axios.get<Diagnosis[]>(`${apiBaseUrl}/diagnoses`);
+        dispatch(setDiagnoses(result.data));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
     if (patientDetails[id]) {
       setPatient(patientDetails[id]);
     } else {
       getPatient();
     }
-    getPatient();
-  }, [id, patientDetails, dispatch],)
+    if(Object.entries(diagnoses).length === 0){
+      fetchDiagnoses();
+    }
 
-  console.log(patient);
-  if(!patient) return <div>Loading</div>;
+  }, [id, patientDetails, dispatch, diagnoses],)
+
+  //console.log(patient);
+  console.log(diagnoses);
+  if(!patient || Object.entries(diagnoses).length === 0) return <div>Loading</div>;
+
+
 
   return(
     <div className="App">
@@ -56,6 +91,14 @@ const PatientPage: React.FC = () => {
       </div>
       <div>
         occupation: {patient.occupation}
+      </div>
+      <div>
+        <h3>entries</h3>
+
+        {patient.entries.map(e =>
+          <EntryRow key={e.id} entry={e} diagnoses={diagnoses}/>
+        )}
+
       </div>
     </div>
   );
